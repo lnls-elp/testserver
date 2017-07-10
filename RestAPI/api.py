5e81f63511219cfb4c1998abd1211ab8067f3e90
allefpablo <allefpablo@gmail.com>
2017-07-10 00:36:40 -0300

    Update. Add Resources for Dcct tables

--- RestAPI/api.py
+++ RestAPI/api.py
@@ -20,28 +20,178 @@ class AddHradc(Resource):
         try:
             #Parse the arguments
             parser = reqparse.RequestParser()
-            parser.add_argument('serial_number', type=int, help='Serial number for equipament')
-            parser.add_argument('type', type=str, help='Type of Equipament HRADC or UDC')
+            #parser.add_argument('numero_serie', type=int, help='Número de série para o equipamento')
+            parser.add_argument('numero_serie', type=int)
+            parser.add_argument('variante', type=str)
+            parser.add_argument('data_instalacao', type=str)
+            parser.add_argument('nome_operador', type=str)
+            parser.add_argument('resistor_burden', type=float)
+            parser.add_argument('frequencia_corte', type=float)
+            parser.add_argument('ordem_filtro', type=int)
+            parser.add_argument('controlador_temperatura', type=int)
+            parser.add_argument('amplificador_burden', type=str)
+            parser.add_argument('jumper_gnd', type=int)
+            parser.add_argument('jumper_burden',type=int)
+            parser.add_argument('filtro_modo_comum', type=int)
             args = parser.parse_args()
 
-            _equipament_serial = args['serial_number']
-            _equipament_type = args['type']
+            _numero_serie           = args['numero_serie']
+            _variante               = args['variante']
+            _data_instal            = args['data_instalacao']
+            _nome_operador          = args['nome_operador']
+            _resistor_burden        = args['resistor_burden']
+            _freq_corte             = args['frequencia_corte']
+            _ordem_filtro           = args['ordem_filtro']
+            _control_temperatura    = args['controlador_temperatura']
+            _amplificador_burden    = args['amplificador_burden']
+            _jumper_gnd             = args['jumper_gnd']
+            _jumper_burden          = args['jumper_burden']
+            _filtro_modo_comum      = args['filtro_modo_comum']
 
             conn = mysql.connect()
             cursor = conn.cursor()
-            cursor.callproc('spAddHradc', (_equipament_serial, _equipament_type))
+            cursor.callproc('sp_add_hradc', (_numero_serie, _variante, _data_instal, _nome_operador,
+                                                _resistor_burden, _freq_corte, _ordem_filtro,
+                                                _control_temperatura, _amplificador_burden, _jumper_gnd,
+                                                _jumper_burden, _filtro_modo_comum))
             data = cursor.fetchall()
 
             if len(data) is 0:
                 conn.commit()
-                return {'StatusCode':'200', 'Message': 'Add Sucess'}
+                return {'StatusCode':'200', 'Message': 'Sucesso'}
             else:
                 return {'StatusCode':'1000', 'Message': str(data[0])}
 
         except Exception as e:
             return {'error': str(e)}
 
+class AddLogHradc(Resource):
+    def post(self):
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('resultado_teste', type=str)
+            parser.add_argument('numero_serie_hradc', type=int)
+            args = parser.parse_args()
+
+            _resultado_teste        = args['resultado_teste']
+            _numero_serie_hradc     = args['numero_serie_hradc']
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_add_log_hradc', (_resultado_teste, _numero_serie_hradc))
+
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return {'StatusCode':'200', 'Message': 'Sucesso'}
+            else:
+                return {'StatusCode':'1000', 'Message': str(data[0])}
+        except Exception as e:
+            return {'error': str(e)}
+
+class AddCalibHradc(Resource):
+    def post(self):
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('temperatura_hradc', type=double)
+            parser.add_argument('temperatura_dmm', type=double)
+            parser.add_argument('temperatura_fonte', type=double)
+            parser.add_argument('nome_operador', type=str)
+            parser.add_argument('ganho_vin', type=double)
+            parser.add_argument('offset_vin', type=double)
+            parser.add_argument('ganho_lin', type=double)
+            parser.add_argument('offset_lin', type=double)
+            parser.add_argument('vref_p', type=double)
+            parser.add_argument('vref_n', type=double)
+            parser.add_argument('gnd', type=double)
+            parser.add_argument('numero_serie_hradc', type=str)
+            args = parser.parse_args()
+
+            _temp_hradc             = args['temperatura_hradc']
+            _temp_dmm               = args['temperatura_dmm']
+            _temp_fonte             = args['temperatura_fonte']
+            _nome_operador          = args['nome_operador']
+            _ganho_vin              = args['ganho_vin']
+            _offset_vin             = args['offset_vin']
+            _ganho_lin              = args['ganho_lin']
+            _offset_lin             = args['offset_lin']
+            _vref_p                 = args['vref_p']
+            _vref_n                 = args['vref_n']
+            _gnd                    = args['gnd']
+            _numero_serie_hradc     = args['numero_serie_hradc']
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_add_calib_hradc', (_temp_hradc, _temp_dmm, _temp_fonte,
+                                                    _nome_operador, ganho_vin, _offset_vin,
+                                                    _ganho_lin, _offset_lin, _vref_p,
+                                                    _vref_n, _gnd, _numero_serie_hradc))
+
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return {'StatusCode':'200', 'Message': 'Sucesso'}
+            else:
+                return {'StatusCode':'1000', 'Message': str(data[0])}
+
+        except Exception as e:
+            return {'erro': str(e)}
+
+class AddDcct(Resource):
+    def post(self):
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('numero_serie', type=int)
+            args = parser.parse_args()
+
+            _numero_serie           = args['numero_serie']
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_add_dcct', (_numero_serie,))
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return {'StatusCode':'200', 'Message': 'Sucesso'}
+            else:
+                return {'StatusCode':'1000', 'Message': str(data[0])}
+
+        except Exception as e:
+            return {'error': str(e)}
+
+class AddLogDcct(Resource):
+    def post(self):
+        try:
+            parser = reqparse.RequestParser()
+            parser.add_argument('resultado_teste', type=str)
+            parser.add_argument('numero_serie_dcct', type=int)
+            args = parser.parse_args()
+
+            _resultado_teste        = args['resultado_teste']
+            _numero_serie_dcct      = args['numero_serie_dcct']
+
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.callproc('sp_add_log_dcct', (_resultado_teste, _numero_serie_dcct))
+
+            data = cursor.fetchall()
+
+            if len(data) is 0:
+                conn.commit()
+                return {'StatusCode':'200', 'Message': 'Sucesso'}
+            else:
+                return {'StatusCode':'1000', 'Message': str(data[0])}
+        except Exception as e:
+            return {'error': str(e)}
+
 api.add_resource(AddHradc, '/AddHradc')
+api.add_resource(AddLogHradc, '/AddLogHradc')
+api.add_resource(AddCalibHradc, '/AddCalibHradc')
+api.add_resource(AddDcct, '/AddDcct')
+api.add_resource(AddLogDcct, '/AddLogDcct')
 
 if __name__ == '__main__':
     #app.run(host='0.0.0.0:5000')
