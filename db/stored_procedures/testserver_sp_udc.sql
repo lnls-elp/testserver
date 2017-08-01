d28164446c0ee87588cfbbcb8b3892e31e7aae46
allefpablo <allefpablo@gmail.com>
2017-08-01 17:30:44 -0300

    Update. New parameters for LogUdc

--- db/stored_procedures/testserver_sp_udc.sql
+++ db/stored_procedures/testserver_sp_udc.sql
@@ -33,7 +33,8 @@ create procedure sp_add_log_udc
 (
     in p_resultado_teste			    varchar(45),
 	in p_numero_serie_udc       	    bigint(15),
-    in p_io_expander                    varchar(45),
+    in p_io_expander0                   varchar(45),
+    in p_io_expander1                   varchar(45),
     in p_leds                           varchar(45),
     in p_buzzer                         varchar(45),
     in p_eeprom                         varchar(45),
@@ -41,9 +42,17 @@ create procedure sp_add_log_udc
     in p_ram                            varchar(45),
     in p_rtc_communication              varchar(45),
     in p_rtc_interrupt                  varchar(45),
-    in p_sensor_temperatura             varchar(45),
+    in p_sensor_temperatura_com         varchar(45),
+    in p_sensor_temperatura_val         varchar(45),
     in p_control_aliment_plano_isolado  varchar(45),
-    in p_rs485                          varchar(45),
+    in p_rs4850                         varchar(45),
+    in p_rs4851                         varchar(45),
+    in p_rs4852                         varchar(45),
+    in p_sdcard_inserido                varchar(45),
+    in p_sdcard_communication           varchar(45),
+    in p_ethernet_inicializacao         varchar(45),
+    in p_ethernet_ping                  varchar(45),
+    in p_loopback                       text,
 	in p_details					    text
 )
 
@@ -53,7 +62,8 @@ insert into LogUdc
 	data,
     resultado_teste,
     numero_serie_udc,
-    io_expander,
+    io_expander0,
+    io_expander1,
     leds,
     buzzer,
     eeprom,
@@ -61,9 +71,17 @@ insert into LogUdc
     ram,
     rtc_communication,
     rtc_interrupt,
-    sensor_temperatura,
+    sensor_temperatura_com,
+    sensor_temperatura_val,
     control_aliment_plano_isolado,
-    rs485,
+    rs4850,
+    rs4851,
+    rs4852,
+    sdcard_inserido,
+    sdcard_communication,
+    ethernet_inicializacao,
+    ethernet_ping,
+    loopback,
 	details
 )
 values
@@ -71,7 +89,8 @@ values
     now(),
     p_resultado_teste,
     p_numero_serie_udc,
-    p_io_expander,
+    p_io_expander0,
+    p_io_expander1,
     p_leds,
     p_buzzer,
     p_eeprom,
@@ -79,10 +98,18 @@ values
     p_ram,
     p_rtc_communication,
     p_rtc_interrupt,
-    p_sensor_temperatura,
+    p_sensor_temperatura_com,
+    p_sensor_temperatura_val,
     p_control_aliment_plano_isolado,
-    p_rs485,
-	p_details
+    p_rs4850,
+    p_rs4851,
+    p_rs4852,
+    p_sdcard_inserido,
+    p_sdcard_communication,
+    p_ethernet_inicializacao,
+    p_ethernet_ping,
+    p_loopback,
+    p_details
 );
 end$$
 delimiter ;
